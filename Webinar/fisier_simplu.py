ca8cb6eac56879bdbc5d5029e4f2662221ceef46
lao.water <baciu.raul555@gmail.com>
2026-09-17 11:43:45 +0300

    refactor: reorganize price calculation and add messages to new text file

--- Webinar/fisier_simplu.py
+++ Webinar/fisier_simplu.py
@@ -1,5 +1,10 @@
-print("Buna din interiorul calculatorului!")
-print("Hello World!")
+# import time
+#
+#
+# print("Reduceri de vara 50%")
+# time.sleep(5)
+# print("Pana in 15 August")
+
 
 
 
@@ -20,10 +25,15 @@ print("Hello World!")
 
 # 5000 de lei
 cont_maria = 5000
-pret_frigider = 2500
+pret_frigider_fara_tva = 2500
+tva = 0.19  # 19%
+
+# Calculăm prețul cu TVA
+pret_frigider = pret_frigider_fara_tva * (1 + tva)
+print(f"Preț fără TVA: {pret_frigider_fara_tva} lei")
+print(f"Preț cu TVA: {pret_frigider} lei")
 
 # Simulam actiunea
 # Maria Cumpara Frigider
 cont_maria = cont_maria - pret_frigider
-
-print(f"Maria are acum in cont: {cont_maria}")
+print(f"Cont Maria după cumpărare: {cont_maria} lei")
